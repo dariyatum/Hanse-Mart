d1c26a77f6d7f9d91704f673ba280da4fa02fe77
dariyatum <dariyatum412@gmail.com>
2025-12-12 08:24:57 +0700

    delete previous commit

--- wp-content/themes/blog-room/inc/frontpage-sections/sections.php
+++
@@ -1,5 +0,0 @@
-<?php
-require get_template_directory() . '/inc/frontpage-sections/flash-posts.php';
-require get_theme_file_path() . '/inc/frontpage-sections/banner.php';
-require get_template_directory() . '/inc/frontpage-sections/categories.php';
-require get_template_directory() . '/inc/frontpage-sections/advertisement.php';
